6b9b6cd134ae1e011a2a90fd660a5d07d58629ae
yifanzheng <zhengyifan1996@outlook.com>
2019-12-09 23:26:02 +0800

    Site updated: 2019-12-09 23:25:59

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -1,1 +1,24 @@
-"use strict";var a_idx=0;jQuery(document).ready(function(o){o("body").click(function(a){var e=new Array("喜欢我","不喜欢我"),t=o("<span/>").text(e[a_idx]),i=a.pageX,n=a.pageY;t.css({"z-index":99999,top:n-28,left:i-8*e[a_idx].length,position:"absolute",color:"#ff7a45"}),o("body").append(t),t.animate({top:n-180,opacity:0},1500,function(){t.remove()}),a_idx=(a_idx+1)%e.length})});
+var a_idx = 0;
+jQuery(document).ready(function($) {
+  $("body").click(function(e) {
+    var a = new Array("喜欢我", "不喜欢我");
+    var $i = $("<span/>").text(a[a_idx]);
+    var x = e.pageX,
+    y = e.pageY;
+    $i.css({
+      "z-index": 99999,
+      "top": y - 28,
+      "left": x - a[a_idx].length * 8,
+      "position": "absolute",
+      "color": "#ff7a45"
+    });
+    $("body").append($i);
+    $i.animate({
+      "top": y - 180,
+      "opacity": 0
+    }, 1500, function() {
+      $i.remove();
+    });
+    a_idx = (a_idx + 1) % a.length;
+  });
+});
